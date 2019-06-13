cde38c32e07cfa5a2a7e559bb91f49bb0934a758
vikas singh kushwah <vikaskushwah312@gmail.com>
2019-06-13 09:34:22 +0530

    medal create to porperty related

---
+++ app/Models/Features.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Features extends Model
+{
+    protected $table = 'features';
+}
---
+++ app/Models/PropertyFeatures.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PropertyFeatures extends Model
+{
+    protected $table = 'property_features';
+}
