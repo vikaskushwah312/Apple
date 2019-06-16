5f2702297fd08038455477aea912a8d8b0784b6a
vikas singh kushwah <vikaskushwah312@gmail.com>
2019-06-16 16:10:16 +0530

    banner image

---
+++ resources/views/web/owner/dashboard/my_properties.blade.php
@@ -0,0 +1,74 @@
+@extends('web.owner.dashboard.master')
+@section('webcontent')
+<div class="dashboard-list">
+    <h3>My Properties List</h3>
+    <table class="manage-table">
+        <tbody>
+        <tr class="responsive-table">
+            <td class="listing-photoo">
+                <img src="http://placehold.it/180x120" alt="listing-photo" class="img-fluid">
+            </td>
+            <td class="title-container">
+                <h2><a href="#">Travel To England</a></h2>
+                <h5 class="d-none d-xl-block d-lg-block d-md-block"><i class="flaticon-pin"></i> 123 Kathal St. Tampa City, </h5>
+                <h6 class="table-property-price">$900 / monthly</h6>
+            </td>
+            <td class="expire-date">4.01.2018</td>
+            <td class="action">
+                <a href="#"><i class="fa fa-pencil"></i> Edit</a>
+                <a href="#"><i class="fa  fa-eye-slash"></i> Hide</a>
+                <a href="#" class="delete"><i class="fa fa-remove"></i> Delete</a>
+            </td>
+        </tr>
+        <tr class="responsive-table">
+            <td class="listing-photoo">
+                <img src="http://placehold.it/180x120" alt="listing-photo" class="img-fluid">
+            </td>
+            <td class="title-container">
+                <h2><a href="#">Amazing Hotel</a></h2>
+                <h5 class="d-none d-xl-block d-lg-block d-md-block"><i class="flaticon-pin"></i> 123 Kathal St. Tampa City, </h5>
+                <h6 class="table-property-price">$900 / monthly</h6>
+            </td>
+            <td class="expire-date">4.01.2018</td>
+            <td class="action">
+                <a href="#"><i class="fa fa-pencil"></i> Edit</a>
+                <a href="#"><i class="fa  fa-eye-slash"></i> Hide</a>
+                <a href="#" class="delete"><i class="fa fa-remove"></i> Delete</a>
+            </td>
+        </tr>
+        <tr class="responsive-table">
+            <td class="listing-photoo">
+                <img src="http://placehold.it/180x120" alt="listing-photo" class="img-fluid">
+            </td>
+            <td class="title-container">
+                <h2><a href="#">Royal Botanic Gardens Sydney</a></h2>
+                <h5 class="d-none d-xl-block d-lg-block d-md-block"><i class="flaticon-pin"></i> 123 Kathal St. Tampa City, </h5>
+                <h6 class="table-property-price">$900 / monthly</h6>
+            </td>
+            <td class="expire-date">4.01.2018</td>
+            <td class="action ">
+                <a href="#"><i class="fa fa-pencil"></i> Edit</a>
+                <a href="#"><i class="fa  fa-eye-slash"></i> Hide</a>
+                <a href="#" class="delete"><i class="fa fa-remove"></i> Delete</a>
+            </td>
+        </tr>
+        <tr class="responsive-table">
+            <td class="listing-photoo">
+                <img src="http://placehold.it/180x120" alt="listing-photo" class="img-fluid">
+            </td>
+            <td class="title-container">
+                <h2><a href="#">Radisson hotel</a></h2>
+                <h5 class="d-none d-xl-block d-lg-block d-md-block"><i class="flaticon-pin"></i> 123 Kathal St. Tampa City, </h5>
+                <h6 class="table-property-price">$900 / monthly</h6>
+            </td>
+            <td class="expire-date">4.01.2018</td>
+            <td class="action">
+                <a href="#"><i class="fa fa-pencil"></i> Edit</a>
+                <a href="#"><i class="fa  fa-eye-slash"></i> Hide</a>
+                <a href="#" class="delete"><i class="fa fa-remove"></i> Delete</a>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
+@stop
