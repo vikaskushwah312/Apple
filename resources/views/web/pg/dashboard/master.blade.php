253d7366ea0c8e44ee127480c937a65793ecb5e7
vikas singh kushwah <vikaskushwah312@gmail.com>
2019-06-23 20:23:37 +0530

    icon image

--- resources/views/web/pg/dashboard/master.blade.php
+++ resources/views/web/pg/dashboard/master.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid">
         <nav class="navbar navbar-expand-lg navbar-light">
             <a class="navbar-brand pad-0" href="{{url('')}}">
-                <img src="{{logoImage(auth('user')->user()->id)}}" alt="logo" height="50px;" width="50px;">
+                {!! logoImage()!!}
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
